9f6620fc4e5acc5ea133e7c9e5f60d485048a807
Amir Kasumovic <amir.kasumovic@live.com>
2023-11-16 16:26:44 +0100

    villa list page added

---
+++ WhiteLagoon/Controllers/VillaController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VillaController
+        public ActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+
+        // GET: VillaController/Details/5
+        public ActionResult Details(int id)
+        {
+            var villa = _context.Villas.FindAsync(id);
+
+            return View();
+        }
+
+        // GET: VillaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VillaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                //var villa = new Villa
+                //{
+
+                //};
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VillaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VillaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                var villa = _context.Villas.FindAsync(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VillaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VillaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                var villa = _context.Villas.FindAsync(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
